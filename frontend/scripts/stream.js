f666e4139fab1536378257e388e5091b5fa7db77
yousuf <gourabxz@gmail.com>
2023-06-26 23:54:37 +0600

    update socket server url

--- frontend/scripts/stream.js
+++ frontend/scripts/stream.js
@@ -1,7 +1,8 @@
 // Viewer client
 
 // get socket server url from env
-const socketServerUrl = 'ws://meets.quarkshub.com:8123';
+const socketServerUrl = 'ws://143.110.246.24:8123';
+// const socketServerUrl = 'ws://localhost:8123';
 // Initialize WebSocket connection
 const socket = new WebSocket(socketServerUrl);
 socket.binaryType = 'arraybuffer';
